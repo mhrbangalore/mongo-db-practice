ee8891c0aa34c9abc88fe77c2cc9b892d7769ac4
mhrbangalore <mkhrnexp@gmail.com>
2025-02-10 00:11:26 +0000

    first commit

---
+++ src/main/java/com/mohan/mongo_db_practice/MongoDbPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.mohan.mongo_db_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohan/mongo_db_practice/MongoDbPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohan.mongo_db_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
